69887aecbfe8afda806a995960c234bf01add8af
Kaede <maplesugano@icloud.com>
2019-02-27 18:45:56 +0000

    フォームデバッグ完了

--- form/views.py
+++ form/views.py
@@ -5,6 +5,7 @@ def form(request):
     d = {
         "scenario" : request.POST.get("scenario"),
         "name1" : request.POST.get("name1"),
-        "role1" : request.POST.get("role1")
+        "role1" : request.POST.get("role1"),
+        # "tbl" : len(participations_table.rows)
     }
     return render(request, 'form/form.html', d)
