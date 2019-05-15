07d745534f67dec4ddab46e41b1f8c7f32105b3a
Kaede Sugano <kaede@sugano.jp>
2019-05-15 16:10:51 +0100

    フォーム基本（テーブルはまだ）

--- form/forms.py
+++ form/forms.py
@@ -1,6 +1,14 @@
 from django import forms
-
+ROLE_CHOICES = (
+    ('protagonist','protagonist'),
+    ('antagonist','antagonist'),
+    ('mentor','mentor'),
+    ('tempter','tempter'),
+    ('skeptic','skeptic'),
+    ('emotional','emotional'),
+    ('logical','logical'),
+)
 class InputScenario(forms.Form):
-     name = forms.CharField()
-     email = forms.EmailField()
-     body = forms.CharField()
+     scenario = forms.CharField(widget=forms.Textarea)
+     characterName = forms.CharField()
+     characterRole = forms.ChoiceField(choices=ROLE_CHOICES)
