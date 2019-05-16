56055565412d224e01b0ccc4cce3a8cc02495632
Kaede Sugano <kaede@sugano.jp>
2019-05-16 16:53:17 +0100

    テーブル追加処理ができないです

--- form/forms.py
+++ form/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 ROLE_CHOICES = (
     ('protagonist','protagonist'),
     ('antagonist','antagonist'),
@@ -8,7 +9,8 @@ ROLE_CHOICES = (
     ('emotional','emotional'),
     ('logical','logical'),
 )
+
 class InputScenario(forms.Form):
-     scenario = forms.CharField(widget=forms.Textarea)
-     characterName = forms.CharField()
-     characterRole = forms.ChoiceField(choices=ROLE_CHOICES)
+     scenario = forms.CharField(widget=forms.Textarea(attrs={'class':'scenario_area'}))
+     characterName = forms.CharField(widget=forms.TextInput(attrs={'size':'30', 'class' : 'name', 'id':'name1'}))
+     characterRole = forms.ChoiceField(choices=ROLE_CHOICES, widget=forms.Select(attrs={'class':'role', 'id':'role1'}))
